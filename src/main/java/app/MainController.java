34375b85b287d222709fbc0f0ab4c285ce81b101
ffellie <ffellie@github.com>
2020-01-29 01:26:42 +0600

    message is now in JSON!

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,5 +1,8 @@
 package app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @Controller
 public class MainController {
@@ -23,9 +28,15 @@ public class MainController {
     public String getMessage (HttpServletResponse response, @RequestParam(name = "id") int id){
         String resp;
         try {
-            resp = mapper.getMessage(id).getMessage();
-
+            Message message = mapper.getMessage(id);
             response.setStatus(200);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                resp = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(message);
+            }
+            catch (JsonProcessingException e){
+                resp = "";
+            }
         }
         catch (NullPointerException e){
             resp = "";
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,6 @@ import '../js/jquery.min.js'
 const text = $("#text")
 
 const base_url = "localhost:8080/"
-$.get("message?id=1", function(data, status){
-          text.append(data);
+$.getJSON("message?id=1", function(data, status){
+          text.append(data.message);
   });
